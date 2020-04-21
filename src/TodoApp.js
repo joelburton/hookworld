9e476fdb7a2fbda76f8aebf6b065fc0612efa38d
Joel Burton <joel@joelburton.com>
2020-04-21 09:09:43 -0700

    Remove outdated reference to uuid

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
 import api from './api';
 import TodoList from './TodoList';
 import NewTodoForm from './NewTodoForm';
